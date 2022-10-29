60c068db06bea6d283d68fc7035649462692ea03
Lajtaib0801 <62424833+Lajtaib0801@users.noreply.github.com>
2022-10-30 00:04:13 +0200

    Add files via upload

--- src/FileManagement.java
+++ src/FileManagement.java
@@ -1,14 +1,25 @@
 import javax.swing.*;
 import java.io.File;
 
+import static java.lang.String.format;
+
 public class FileManagement {
-    final String defPath = "C:\\";
+    final String defPath = (OsType().toLowerCase().contains("windows") ? "C:\\" : format("/home/%s",System.getProperty("user.name")));
+
     public DefaultListModel Filelist() {
         DefaultListModel listmodel = new DefaultListModel();
         File folder = new File(defPath);
-        for (int i = 0; i < folder.listFiles().length; i++) {
-            listmodel.add(i,folder.listFiles()[i]);
+        if (folder.exists()) {
+            for (int i = 0; i < folder.listFiles().length; i++) {
+                listmodel.add(i, folder.listFiles()[i]);
+            }
+        } else {
+            listmodel.add(0, format("This file: '%s' does not exist!", defPath));
         }
         return listmodel;
+    }
+
+    public String OsType() {
+        return System.getProperty("os.name");
     }
 }
